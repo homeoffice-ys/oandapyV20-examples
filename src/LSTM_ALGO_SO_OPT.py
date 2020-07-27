3178385107dcecabddbb532682e7cfd2364ccd18
Yochanan <yochanan.steinberg@gmail.com>
2020-07-27 08:57:07 -0400

    debug df['high']

--- src/LSTM_ALGO_SO_OPT.py
+++ src/LSTM_ALGO_SO_OPT.py
@@ -6,18 +6,20 @@ import matplotlib.pyplot as plt
 import json
 import os
 import datetime
-import re
 from ta import add_all_ta_features
+import ta
 from pyts.decomposition import SingularSpectrumAnalysis
 
+def Stoch(close,high,low):
+    slowk, slowd = ta.STOCH(high, low, close)
+    stochSell = ((slowk < slowd) & (slowk.shift(1) > slowd.shift(1))) & (slowd > 80)
+    stochBuy = ((slowk > slowd) & (slowk.shift(1) < slowd.shift(1))) & (slowd < 20)
+    return stochSell, stochBuy, slowk, slowd
 
 instrument = 'EUR_USD'
 granularity = 'M1'
 pt = PriceTable(instrument, granularity)
 cf = PRecordFactory(pt.granularity)
-ask = []
-bid = []
-t = []
 data = []
 
 # to retrieve:
@@ -29,40 +31,32 @@ for tick in my_list:
     # print(tick)
     rec = cf.parseTick(tick)
     if rec:
-        # t.append(rec[0])
-        # tmp = (datetime.datetime(*map(int, re.split('[^\d]', rec[0])[:-1])))
-        # print(rec[0])
         tmp = datetime.datetime.strptime(rec[0], '%Y-%m-%dT%H:%M:%S.%fZ')
-        # ask.append(rec[1])
-        # bid.append(rec[6])
-        data.append([tmp, rec[1], rec[6]])
-        # data.append([tmp])
+        price = (rec[1] + rec[6])/2
+        spread = (rec[1] - rec[6])
+        # data.append([tmp, rec[1], rec[6]])
+        data.append([tmp, price, spread])
 
-
-
-df = pd.DataFrame(data, columns=['Time', 'Ask', 'Bid'], dtype=float)
-# df = pd.DataFrame(data, columns=['Time'], dtype=str)
-print(df.head(5))
-
-# ticks = df.ix[:, ['Ask', 'Bid']]
-# bars = ticks.resample('30min').ohlc()
-# df.index = pd.to_datetime(df.index, unit='s')
-# df["Date"] = pd.to_datetime(df["Date"])
+df = pd.DataFrame(data, columns=['Time', 'Price', 'Spread'], dtype=float)
 df = df.set_index(pd.to_datetime(df['Time']))
-# print(df.head(5))
-# exit()
-
-data_ask = df['Ask'].resample('15Min').ohlc()
-data_bid = df['Bid'].resample('15Min').ohlc()
-
-print(data_bid.head(5))
-
+data_price = df['Price'].resample('15Min').ohlc()
 '''
 For smaller time frames 
 (milliseconds/microseconds/seconds), use L for milliseconds, 
 U for microseconds, and S for seconds.
 '''
-df = pd.concat([data_ask, data_bid], axis=1, keys=['Ask', 'Bid'])
+# df = pd.concat([data_price], axis=1, keys=['Price'])
+df = pd.concat([data_price], axis=1)
 print(df.head(5))
-
+print(df['high'])
+'''
+class
+    ta.momentum.StochasticOscillator(
+    high: pandas.core.series.Series, 
+    low: pandas.core.series.Series, 
+    close: pandas.core.series.Series, 
+    n: int = 14, d_n: int = 3, fillna: bool = False)
+'''
+SO = ta.momentum.StochasticOscillator(df['high'], df['low'], df['close'], n=14, d_n=3)
+print(SO)
 
